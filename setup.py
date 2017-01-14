8394d30ab644292ca5608233313296e5147f8ecf
Kyle Hickmann <hickmank@lanl.gov>
2017-01-14 14:28:11 -0700

    Updated setup.py version information

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-###############################################################################
-###############################################################################
-#   Copyright 2014 Kyle S. Hickmann and
-#                  The Administrators of the Tulane Educational Fund
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-###############################################################################
-###############################################################################
-
 import os
 
 from ez_setup import use_setuptools
@@ -29,14 +10,14 @@ def read(fname):
 
 setup(
 	name = 'pyda',
-	version = '1.0',
-	description = 'pyda is a general object-oriented data assimilation package',
+	version = '1.0.1',
+	description = "pyda: An object-oriented data assimilation package",
 	author = 'Kyle Hickmann',
-	author_email = 'khickma@tulane.edu',
+	author_email = 'hickmank@gmail.com',
 	maintainer = 'Kyle Hickmann',
-	maintainer_email = 'khickma@tulane.edu',
+	maintainer_email = 'hickmank@gmail.com',
 	url = 'https://hickmank.github.io/pyda',
-        long_description = read('README'),
+        long_description = read('README.md'),
 	license = 'Apache 2.0',
 	classifiers = [
                 'Development Status :: 3 - Alpha',
